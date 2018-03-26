285abfb06eff905907687a2eb9e45df0a535f2fe
kgsoft <kgsoft@871d6764-1e22-0410-b313-a55050885396>
2018-03-26 05:41:29 +0000

    アウトラインプラグインのページ指定対応

--- plugin/info/Outline.pm
+++ plugin/info/Outline.pm
@@ -4,6 +4,13 @@
 # <pre>
 # {{outline}}
 # </pre>
+# <p>
+# デフォルトでは表示中のページのアウトラインを表示しますが、
+# 指定したページのアウトラインを表示する事も出来ます。
+# </p>
+# <pre>
+# {{outline ページ名}}
+# </pre>
 #
 ############################################################
 package plugin::info::Outline;
@@ -25,16 +32,20 @@ sub new {
 sub paragraph {
 	my $self = shift;
 	my $wiki = shift;
+	my $page = shift;
 	my $cgi  = $wiki->get_CGI;
 	my $p_cnt = 0;
 	
-	my $pagename = $cgi->param("page");
+	my $pagename = $page;
+	if (!defined($pagename)) {
+		$pagename = $cgi->param("page");
+	}
 	# ページの参照権限があるかどうか調べる
 	unless($wiki->can_show($pagename)){
 		return undef;
 	}
 	my $parser = plugin::info::OutlineParser->new($wiki);
-	return $parser->outline($wiki->get_page($pagename));
+	return $parser->outline($wiki->get_page($pagename), $pagename);
 }
 
 1;
